bed3b4c0c6ac44374bf88b1c408fc9f691dd7bec
xlli3 <xlli3@student.unimelb.edu.au>
2020-10-24 15:34:27 +1100

    Initializing MediBooking server project.

---
+++ src/main/java/com/medibooking/bookingserviceserver/BookingServiceServerApplication.java
@@ -0,0 +1,13 @@
+package com.medibooking.bookingserviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medibooking/bookingserviceserver/BookingServiceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibooking.bookingserviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
